c924fe5a37fc39d08b25bca8ed48c1c778c27a9a
hugo <hugo.debosschere@student-cs.fr>
2026-02-04 14:24:38 +0100

    modif

--- pytorch_template_code/submit-slurm-no-tmp.py
+++ pytorch_template_code/submit-slurm-no-tmp.py
@@ -11,10 +11,12 @@ def makejob(commit_id, configpath, nruns):
 
 #SBATCH --job-name=autosend
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_prod_long
+#SBATCH --partition=gpu_inter
 #SBATCH --time=48:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
+#SBATCH --exclude=sh
+#SBATCH --exclude=cam
 #SBATCH --array=1-{nruns}
 
 
