f192a629beb9f4b9e0ee811b76d7f45c992f0a06
hugo.debosschere@student-cs.fr <hugodebosschere@centralesupelec.fr>
2026-02-16 16:51:48 +0100

    only AdamW for now, maybe Muon later

--- pytorch_template_code/submit-slurm-dgx.py
+++ pytorch_template_code/submit-slurm-dgx.py
@@ -11,10 +11,10 @@ def makejob(commit_id, configpath, nruns):
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:3g.40gb:1
-#SBATCH --job-name=focalloss
+#SBATCH --gres=gpu:A100.80gb:1
+#SBATCH --job-name=sweep
 #SBATCH --nodes=1
-#SBATCH --partition=prod40
+#SBATCH --partition=prod80
 #SBATCH --time=24:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
