1845faee6eeca7c261251e69ad5ec89fd5c09911
hugo <hugo.debosschere@student-cs.fr>
2026-02-18 11:21:07 +0100

    modif submit slurm pour pretrained

--- pytorch_template_code/submit-slurm.py
+++ pytorch_template_code/submit-slurm.py
@@ -9,13 +9,13 @@ import tempfile
 def makejob(commit_id, configpath, nruns):
     return f"""#!/bin/bash
 
-#SBATCH --job-name=autosend
+#SBATCH --job-name=pretrained
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_prod_long
 #SBATCH --time=48:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
-#SBATCH --exclude=sh[00,05,03,10-22]    
+#SBATCH --exclude=sh[00,01,05,03,10-19,20]    
 #SBATCH --array=1-{nruns}
 
 
