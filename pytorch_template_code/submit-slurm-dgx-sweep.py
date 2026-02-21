c353016dee30c01b5ba071e5ed161ffba90c195b
hugo.debosschere@student-cs.fr <hugodebosschere@dgx-a100>
2026-02-21 15:36:26 +0100

    launching prod40 for hyperband

--- pytorch_template_code/submit-slurm-dgx-sweep.py
+++ pytorch_template_code/submit-slurm-dgx-sweep.py
@@ -9,12 +9,12 @@ import tempfile
 def makejob(commit_id, configpath, nruns):
     return f"""#!/bin/bash
 #SBATCH --mem=64G
-#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:nvidia_a100_3g.40gb
 #SBATCH --job-name=40hypers
 #SBATCH --nodes=1
-#SBATCH --partition=prod80
+#SBATCH --partition=prod40
 #SBATCH --time=24:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
