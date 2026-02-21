923b0724b47f8618ffd92837756c78991aedebe6
hugo.debosschere@student-cs.fr <hugodebosschere@dgx-a100>
2026-02-21 15:53:15 +0100

    80 hyper

--- pytorch_template_code/submit-slurm-dgx-sweep.py
+++ pytorch_template_code/submit-slurm-dgx-sweep.py
@@ -11,10 +11,10 @@ def makejob(commit_id, configpath, nruns):
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=64
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:nvidia_a100_3g.40gb
-#SBATCH --job-name=40hypers
+#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb
+#SBATCH --job-name=80hypers
 #SBATCH --nodes=1
-#SBATCH --partition=prod40
+#SBATCH --partition=prod80
 #SBATCH --time=24:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
