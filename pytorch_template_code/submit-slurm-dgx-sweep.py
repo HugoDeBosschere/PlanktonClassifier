359cdd0da6dcd7eeb6f7e08cc2b96e7d7184d002
hugo.debosschere@student-cs.fr <hugodebosschere@dgx-a100>
2026-02-21 15:10:59 +0100

    hyperband dgx

--- pytorch_template_code/submit-slurm-dgx-sweep.py
+++ pytorch_template_code/submit-slurm-dgx-sweep.py
@@ -14,7 +14,7 @@ def makejob(commit_id, configpath, nruns):
 #SBATCH --gres=gpu:nvidia_a100_3g.40gb:1
 #SBATCH --job-name=40hyper
 #SBATCH --nodes=1
-#SBATCH --partition=prod80
+#SBATCH --partition=prod40
 #SBATCH --time=24:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
